7eaad6cf5c94d446a32cde70e39e59ef880476f1
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-08-04 23:10:49 -0700

    Define package interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export * from './model'
+export * from './diagnostic'
+export { FileProvider } from './file'
 export { parseEpub } from './parse'
+export { openEpub } from './open'
+export {
+    validateEpub, validateContainer, validatePackageDocument,
+    validateNavDocument, validateNcxDocument,
+} from './validate'
