c0da16d223aba066d06f222870ee5502da73da1d
Anton Shakirzianov <ashakirzianov@icloud.com>
2025-05-10 17:01:28 -0300

    Rollback last xml type change

--- src/xml.ts
+++ src/xml.ts
@@ -8,9 +8,8 @@ export type XmlAttributes = {
 export type XmlText = {
     '#text'?: string | undefined,
 }
-type FirstSymbol = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | '_'
 export type XmlContainer = {
-    [key in `${FirstSymbol}${string}`]: XmlNode[];
+    [key in string]: XmlNode[];
 }
 
 // TODO: change to better type
